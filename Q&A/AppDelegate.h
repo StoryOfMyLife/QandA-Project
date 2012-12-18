c7726b93ed6ea171ecb20b478992e602f5271b1c
aalkdf <282503827@qq.com>
2012-12-18 18:29:55 +0800

    Initial Commit

---
+++ Q&A/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Q&A
+//
+//  Created by 刘廷勇 on 12-12-18.
+//  Copyright (c) 2012年 刘廷勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Q&A/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Q&A
+//
+//  Created by 刘廷勇 on 12-12-18.
+//  Copyright (c) 2012年 刘廷勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
